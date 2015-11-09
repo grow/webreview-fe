2cdf58492748276a9cd5c339b4f3f2675460d8fb
Jeremy Weinstein <jeremydw@gmail.com>
2015-11-08 22:26:57 -0800

    Translation dashboard.

--- app/models/catalog.js
+++ app/models/catalog.js
@@ -2,6 +2,11 @@ import DS from 'ember-data';
 
 export default DS.Model.extend({
   locale: DS.attr('string'),
+  name: DS.attr('string'),
+  percent_translated: DS.attr('number'),
+  num_fuzzy: DS.attr('number'),
+  num_translated: DS.attr('number'),
+  num_messages: DS.attr('number'),
   project: DS.belongsTo('project'),
   translations: DS.hasMany('translation'),
 });
