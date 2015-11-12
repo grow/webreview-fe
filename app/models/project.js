bb1a33f6e2bdcf5c58863c35f989d63b35bf1c60
Jeremy Weinstein <jeremydw@gmail.com>
2015-11-12 01:52:31 -0800

    Repo link status.

--- app/models/project.js
+++ app/models/project.js
@@ -2,6 +2,7 @@ import DS from 'ember-data';
 
 export default DS.Model.extend({
   avatar_url: DS.attr('string'),
+  buildbot_job_id: DS.attr('string'),
   catalogs: DS.hasMany('catalog'),
   description: DS.attr('string'),
   git_url: DS.attr('string'),
